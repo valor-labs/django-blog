11fe728c63052c84f7ed25663ff55135827d3ed0
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2024-04-22 14:58:22 +0300

    update dependencies and set en admin lang

--- blogyek/settings.py
+++ blogyek/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Istanbul'
 
